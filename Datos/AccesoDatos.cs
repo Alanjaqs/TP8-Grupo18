b1addaedebc2d4af455bb3e0e75990dc5a7172fe
Alanjaqs <alannjaqs@gmail.com>
2025-05-30 23:54:28 -0300

    Primer commit

---
+++ Datos/AccesoDatos.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos
+{
+    public class AccesoDatos
+    {
+        string conexionBDSucursales = "Data Source=localhost\\sqlexpress;Initial Catalog=BDSucursales;Integrated Security=True";
+
+        public SqlConnection ObtenerConexion()
+        {
+            SqlConnection conexion = new SqlConnection(conexionBDSucursales);
+
+            try
+            {
+                conexion.Open();
+                return conexion;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+    }
+}
