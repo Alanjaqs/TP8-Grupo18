e808c735b83dab99d7e4943fce5fcd4899af6eee
Alanjaqs <alannjaqs@gmail.com>
2025-05-31 22:18:47 -0300

    Empiezo ejercicio 3

---
+++ Negocio/NegocioSucursal.cs
@@ -0,0 +1,21 @@
+﻿using Datos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocio
+{
+    public class NegocioSucursal
+    {
+        public int EliminarSucursal(int IdSucursal)
+        {
+            int filasAfectadas;
+            AccesoDatos datos = new AccesoDatos();
+            filasAfectadas = datos.EjecutarTransaccion("DELETE FROM Sucursal WHERE Id_Sucursal = " + IdSucursal, datos.ObtenerConexion());
+            return filasAfectadas;
+        }
+
+    }
+}
