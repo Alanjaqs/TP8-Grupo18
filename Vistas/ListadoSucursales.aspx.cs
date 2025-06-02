6bae4379cc29a4fb87955122adf7bf8044ebf476
Jonathan1993x <ssilguerojonathan@gmail.com>
2025-06-02 12:42:48 -0300

    ObtenerTablasSucursales

--- Vistas/ListadoSucursales.aspx.cs
+++ Vistas/ListadoSucursales.aspx.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Negocio;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -9,9 +11,16 @@ namespace Vistas
 {
     public partial class ListadoSucursales : System.Web.UI.Page
     {
+        NegocioSucursal negocio = new NegocioSucursal();
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            UnobtrusiveValidationMode = UnobtrusiveValidationMode.None;
+            if (!IsPostBack)
+            {
+                DataTable tabla = negocio.ObtenerTablaSucursales();
+                gvSucursales.DataSource = tabla;
+                gvSucursales.DataBind();
+            }
         }
 
         protected void Button1_Click(object sender, EventArgs e)
